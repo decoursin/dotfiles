f8a392de8a697c2ac1980d2e7da27447ba640a81
Nick <ndecoursin@gmail.com>
2015-01-25 14:36:48 -0600

    Create make.sh file for creating symlinks.

---
+++ make.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+######### Variables
+
+dir=$HOME/dotfiles
+old_dotfiles=$HOME/dotfiles_old
+
+shopt -s nullglob
+files=( ${dir}/* )
+
+#########
+
+######## Functions
+
+make_symlinks() {
+	read -p "Symlink .${filename}?" -n 1 -r
+	if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+		echo 
+		continue
+	fi
+	echo    # (optional) move to a new line
+	echo "Moving '$home_file' to '$old_dotfiles/$filename'"
+	mv -T "$home_file" "${old_dotfiles}/${filename}"
+	echo -e "Creating symlink from '$src' to '$home_file' \n"
+	ln -s "$src" "$home_file"
+}
+
+#########
+
+# Makesure the dotfiles directory exists and there is files in it
+if [[ ! -d ${dir} && ${#files[@]} -eq 0 ]]; then
+	echo "Tbe ${dir} directory does not exist yet or it's empty. Please clone it first."
+fi
+
+# Create a ~/dotfiles_old directory
+if [[ ! -d ${old_dotfiles} ]]; then
+	if mkdir "$old_dotfiles"; then
+		echo "Created '${old_dotfiles}' directory."
+	else
+		echo "Could not create ${old_dotfiles}." >&2
+		exit 1
+	fi
+fi
+
+# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+for src in "${files[@]}"; do
+	filename="$(basename "$src")"
+	home_file="$HOME/.${filename}"
+	if [[ -f "$src" || -d "$src" ]] && [ "$filename" != "$(basename "$0")" ]; then
+		make_symlinks
+	fi
+done
+
+echo "Finished!"
+exit 0

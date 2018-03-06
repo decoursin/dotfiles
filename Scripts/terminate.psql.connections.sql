58d6a49e823f861894f810fd758bdd8ca9e24a9f
decoursin <ndecoursin@gmail.com>
2018-03-06 15:44:52 +0100

    terminate psql connections sql

---
+++ Scripts/terminate.psql.connections.sql
@@ -0,0 +1,7 @@
+-- taken from here:
+-- https://stackoverflow.com/questions/5108876/kill-a-postgresql-session-connection
+
+SELECT *, pg_terminate_backend(pid)
+FROM pg_stat_activity 
+WHERE pid <> pg_backend_pid()
+AND datname = 'my_database_name';
